6640e0b06c5b5ae5675f87adcb2129bc0ba16293
Rene Bonilla <rbonillajr@hotmail.com>
2015-11-16 23:08:53 -0500

    fix css

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ UGLIFY = node_modules/.bin/uglifyjs
 
 all: \
 	$(shell npm install && mkdir -p dist) \
-	dist/leaflet.buffer.css \
+	dist/Leaflet.buffer.css \
 	dist/Leaflet.buffer.js \
 	dist/Leaflet.buffer.min.js \
 	dist/buffer.png
@@ -15,8 +15,8 @@ dist/buffer.png: src/buffer.png
 	cp src/buffer.png dist/buffer.png
 	cp src/buffer@2x.png dist/buffer@2x.png
 
-dist/leaflet.buffer.css: src/leaflet.buffer.css
-	cp src/leaflet.buffer.css dist/leaflet.buffer.css
+dist/Leaflet.buffer.css: src/Leaflet.buffer.css
+	cp src/Leaflet.buffer.css dist/Leaflet.buffer.css
 
 dist/Leaflet.buffer.js: src/Leaflet.buffer.js
 	cp src/Leaflet.buffer.js dist/Leaflet.buffer.js
--- dist/Leaflet.buffer.js
+++ dist/Leaflet.buffer.js
@@ -1,39 +1,83 @@
 L.Control.Buffer = L.Control.extend({
     options: {
         position: 'topleft',
-        title: {
-            'false': 'View buffer',
-            'true': 'Exit buffer'
-        }
+        modal: false
     },
 
     onAdd: function (map) {
-        var container = L.DomUtil.create('div', 'leaflet-control-buffer leaflet-bar leaflet-control');
+        this._map = map;
+        this._active = false;
+        this._container = L.DomUtil.create('div', 'leaflet-control-buffer leaflet-bar leaflet-control');
+        this._container.title = "Buffer to specific area";
+        this._link = L.DomUtil.create('a', 'leaflet-control-buffer-button leaflet-bar-part', this._container);
+        this._link.href = '#';
 
-        this.link = L.DomUtil.create('a', 'leaflet-control-buffer-button leaflet-bar-part', container);
-        this.link.href = '#';
-
-        // this._map = map;
+        
         // this._map.on('bufferchange', this._toggleTitle, this);
         // this._toggleTitle();
 
-        L.DomEvent.on(this.link, 'click', this._click, this);
+        //L.DomEvent.on(this._link, 'click', this._click, this);
+        
+        var _origClick = map.buffer._onClick;
+        map.buffer._onClick = function(e){
+          _origClick.call(map.buffer, {
+                clientX: e.clientX,
+                clientY: e.clientY,
+                which: 1,
+                shiftKey: true
+            });  
+        };
+        
+        map.on('click', function(){
+            alert('clicked on map');
+        }, this);
+        
+        if (!this.options.modal) {
+            map.on('bufferend', this.deactivate, this);
+        }
+        
+        L.DomEvent
+            .on(this._container, 'dblclick', L.DomEvent.stop)
+            .on(this._container, 'click', L.DomEvent.stop)
+            .on(this._container, 'click', function(){
+                this._active = !this._active;
+                if (this._active && map.getZoom() != map.getMaxZoom()){
+                    this.activate();
+                }
+                else {
+                    this.deactivate();
+                }
+            }, this);
 
-        return container;
+        return this._container;
+    },    
+    activate: function() {
+        L.DomUtil.addClass(this._container, 'active');
+         this._map.dragging.disable();
+        // this._map.buffer.addHooks();
+        L.DomUtil.addClass(this._map.getContainer(), 'leaflet-control-buffer-crosshair');
     },
-
+    deactivate: function() {
+        L.DomUtil.removeClass(this._container, 'active');
+         this._map.dragging.enable();
+        // this._map.buffer.removeHooks();
+        L.DomUtil.removeClass(this._map.getContainer(), 'leaflet-control-buffer-crosshair');
+        this._active = false;
+        
+    },
     _click: function (e) {
         L.DomEvent.stopPropagation(e);
-        L.DomEvent.preventDefault(e);
-       // this._map.toggleBuffer();
+        L.DomEvent.preventDefault(e);    
     },
-
-    // _toggleTitle: function() {
-    //     this.link.title = this.options.title[this._map.isbuffer()];
-    // }
+    
+    addTo: function (map) {
+			L.Control.prototype.addTo.call(this, map);		
+			return this;
+		}
+    
 });
 
 
-L.control.Buffer = function (options) {
-    return new L.Control.buffer(options);
+L.control.buffer = function (options) {
+    return new L.Control.Buffer(options);
 };
